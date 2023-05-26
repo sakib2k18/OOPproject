d14bd86eeaec03e6c6a75af342f9d761559e26e3
sakib <nafiulsakib2k18@gmail.com>
2023-05-26 20:52:36 +0600

    Updated

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -36,26 +36,14 @@ public:
     }
 };
 
-class Warden : public Person {
-private:
-    string designation;
-
-public:
-    Warden(const string& _name, int _age, char _gender, const string& _designation)
-        : Person(_name, _age, _gender), designation(_designation) {}
-
-    void displayRole() const override {
-        cout << "Warden" << endl;
-    }
-};
-
 class Staff : public Person {
 private:
-    string role;
+    string job;
+    char shift;
 
 public:
-    Staff(const string& _name, int _age, char _gender, const string& _role)
-        : Person(_name, _age, _gender), role(_role) {}
+    Staff(const string& _name, int _age, char _gender, const string& _job, char _shift)
+        : Person(_name, _age, _gender), job(_job), shift(_shift) {}
 
     void displayRole() const override {
         cout << "Staff" << endl;
@@ -66,7 +54,7 @@ class Room {
 private:
     int roomNumber;
     int capacity;
-    vector<Person*> occupants;
+    vector<Person*> borders;
 
 public:
     Room(int _roomNumber, int _capacity)
@@ -80,17 +68,17 @@ public:
         return capacity;
     }
 
-    int getOccupancy() const {
-        return occupants.size();
+    int getvacancy() const {
+        return borders.size();
     }
 
     bool isAvailable() const {
-        return occupants.size() < capacity;
+        return borders.size() < capacity;
     }
 
-    bool addOccupant(Person* person) {
-        if (occupants.size() < capacity) {
-            occupants.push_back(person);
+    bool addborder(Person* person) {
+        if (borders.size() < capacity) {
+            borders.push_back(person);
             return true;
         }
         return false;
@@ -99,10 +87,10 @@ public:
     friend ostream& operator<<(ostream& os, const Room& room) {
         os << "Room Number: " << room.roomNumber << endl;
         os << "Capacity: " << room.capacity << endl;
-        os << "Occupancy: " << room.occupants.size() << endl;
-        os << "Occupants: ";
-        for (const auto& occupant : room.occupants) {
-            os << occupant->getName() << " ";
+        os << "Vacancy: " << room.borders.size() << endl;
+        os << "borders: ";
+        for (const auto& border : room.borders) {
+            os << border->getName() << " ";
         }
         os << endl;
         return os;
@@ -113,16 +101,11 @@ class Hostel {
 private:
     vector<Room> rooms;
     vector<Student> students;
-    vector<Warden> wardens;
     vector<Staff> staff;
 
 public:
     void addStudent(const Student& student) {
         students.push_back(student);
-    }
-
-    void addWarden(const Warden& warden) {
-        wardens.push_back(warden);
     }
 
     void addStaff(const Staff& staffMember) {
@@ -132,13 +115,13 @@ public:
     bool assignRoomToStudent(const Student& student, int roomNumber) {
         for (auto& room : rooms) {
             if (room.getRoomNumber() == roomNumber && room.isAvailable()) {
-                return room.addOccupant(new Student(student));
+                return room.addborder(new Student(student));
             }
         }
         return false;
     }
 
-    void displayRoomInformation(int roomNumber) const {
+    void displayRoomInfo(int roomNumber) const {
         for (const auto& room : rooms) {
             if (room.getRoomNumber() == roomNumber) {
                 cout << room;
@@ -152,30 +135,20 @@ public:
 int main() {
     Hostel hostel;
 
-    // Add students
     Student student1("John Doe", 20, 'M', 101);
     Student student2("Jane Smith", 19, 'F', 102);
     hostel.addStudent(student1);
     hostel.addStudent(student2);
 
-    // Add wardens
-    Warden warden1("David Brown", 35, 'M', "Senior Warden");
-    Warden warden2("Emily Johnson", 40, 'F', "Assistant Warden");
-    hostel.addWarden(warden1);
-    hostel.addWarden(warden2);
-
-    // Add staff
-    Staff staff1("Alex Wilson", 28, 'M', "Security");
-    Staff staff2("Olivia Davis", 25, 'F', "Housekeeping");
+    Staff staff1("Alex Wilson", 28, 'M', "Security", 'N');
+    Staff staff2("Olivia Davis", 25, 'F', "Housekeeping", 'D');
     hostel.addStaff(staff1);
     hostel.addStaff(staff2);
 
-    // Assign rooms to students
     hostel.assignRoomToStudent(student1, 101);
     hostel.assignRoomToStudent(student2, 102);
 
-    // Display room information
-    hostel.displayRoomInformation(101);
+    hostel.displayRoomInfo(101);
 
     return 0;
 }
